040dd3803321ed0d9b956c5615a868f3be40bb83
betmen657123789-web <betmen657123789@gmail.com>
2026-06-06 20:43:05 +0800

    first implementation

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
 import asyncio
-from telegram.handlers import prices
-from telegram.bot import bot, dp
-from telegram.handlers.about import router as about_router
+from app.telegram.handlers import prices
 
+from app.telegram.bot import (
+    bot,
+    dp
+)
 dp.include_router(prices.router)
-dp.include_router(about_router)
+from app.telegram.handlers.about import (
+    router as about_router
+)
+
+dp.include_router(
+    about_router
+)
+
+
 
 async def main():
+
     print("Bot started")
+
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
+
     asyncio.run(main())
