00804b8bb5df090a9dc9a48388c8b9bef14eace9
betmen657123789-web <betmen657123789@gmail.com>
2026-06-13 02:30:27 +0800

    first implementation

--- app/main.py
+++ app/main.py
@@ -5,10 +5,24 @@ from app.telegram.bot import bot, dp
 from app.telegram.handlers import prices
 from app.telegram.handlers.about import router as about_router
 
+from app.database.base import Base
+from app.database.session import engine
+
+# Важно: импортируем модели, чтобы SQLAlchemy их увидел
+from app.database.models.user import User
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
 
 async def main():
 
-    # подключаем доп. роутеры (которые не подключены в bot.py)
+    # Создаём таблицы если их нет
+    await create_tables()
+
+    # Подключаем роутеры
     dp.include_router(prices.router)
     dp.include_router(about_router)
 
